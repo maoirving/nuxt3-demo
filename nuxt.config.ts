99715656f8535964787a52867dc7f5e9f01da2d9
mao <1559304351@qq.com>
2022-05-22 11:22:05 +0800

    chore: set ssr config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: true, // default: true
   modules: ['@vueuse/nuxt'],
   css: ['@/assets/sass/app.scss'],
   vite: {
