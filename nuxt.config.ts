a452d52145f5ac6455f0d5e49edfcbf3c487d7be
mao <1559304351@qq.com>
2022-05-22 11:21:00 +0800

    feat: support global css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,14 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@vueuse/nuxt'],
+  css: ['@/assets/sass/app.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/sass/utils/variables.scss";',
+        },
+      },
+    },
+  },
 })
