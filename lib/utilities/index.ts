4c45ca5caddf887cc71bb355d170f3698cfa2ee3
mao <1559304351@qq.com>
2022-05-22 17:17:16 +0800

    feat: add some array utilities

---
+++ lib/utilities/index.ts
@@ -0,0 +1,67 @@
+import { Dictionary, clone } from 'ramda'
+import { Action, ObjectAction } from './types'
+
+// Array
+export function parseToArray<T>(val: T | T[]) {
+  if (!val) return []
+
+  return Array.isArray(val) ? val : [val]
+}
+
+export function unique<T>(arr: T[]) {
+  return Array.from(new Set(arr))
+}
+
+export const uniqueBy = (arr: Dictionary<any>[], propName: string) => {
+  // can alse use uniqBy from 'lodash'
+  const set = new Set([])
+  const newArr = []
+
+  arr.forEach(obj => {
+    const val = obj[propName]
+    if (set.has(val)) return
+    newArr.push(obj)
+    set.add(val)
+  })
+
+  return newArr
+}
+
+// Object
+
+export const combineObject = (
+  currentObj: Dictionary<string | string[]>,
+  objActions: ObjectAction | ObjectAction[]
+) => {
+  const newObj = clone(currentObj)
+  const objActionArr = parseToArray(objActions)
+
+  objActionArr.forEach(item => {
+    const key = item.key
+    let newValue: string[] = []
+    const newObjVals = parseToArray(newObj[key])
+    const valuesArr = parseToArray(item.value)
+    const valuesArrSet = new Set(valuesArr)
+
+    switch (item.action) {
+      case Action.Remove:
+        newValue = newObjVals.filter(val => !valuesArrSet.has(val))
+        break
+      case Action.Replace:
+        newValue = valuesArr
+        break
+      case Action.Delete:
+        break
+      default:
+        newValue = unique(newObjVals.concat(valuesArr))
+    }
+
+    if (newValue.length) {
+      newObj[key] = newValue.length > 1 ? newValue : newValue?.[0]
+    } else {
+      delete newObj[key]
+    }
+  })
+
+  return newObj
+}
---
+++ lib/utilities/types/index.ts
@@ -0,0 +1,12 @@
+export enum Action {
+  Add = 0,
+  Remove = 1,
+  Replace = 2,
+  Delete = 3,
+}
+
+export interface ObjectAction {
+  key: string
+  value?: string | string[]
+  action: Action
+}
