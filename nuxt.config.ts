19d8b76ff3f1305867fb0a991a279b53da0486ff
mao <1559304351@qq.com>
2022-05-22 15:43:13 +0800

    chore: support to get data from origin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,18 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: true, // default: true
+  ssr: process.env.SSR !== 'false', // default: true
   modules: ['@vueuse/nuxt'],
   css: ['@/assets/sass/app.scss'],
   vite: {
+    server: {
+      proxy: {
+        '/api/': {
+          target: process.env.API_ENDPOINT,
+          changeOrigin: true,
+        },
+      },
+    },
     css: {
       preprocessorOptions: {
         scss: {
