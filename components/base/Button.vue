924937414604cf4119cb72a94b089efd834319c5
mao <1559304351@qq.com>
2022-06-04 19:30:53 +0800

    feat: extend base button

--- components/base/Button.vue
+++ components/base/Button.vue
@@ -1,16 +1,63 @@
-<script lang="ts" setup>
-const buttonType = computed<'button' | 'submit' | 'reset'>(() => 'button')
-const emit = defineEmits(['click'])
+<script lang="ts">
+import { PropType } from 'vue'
+import { RouteLocationRaw } from 'vue-router'
 
-const onClick = () => {
-  emit('click')
-}
+export default defineComponent({
+  inheritAttrs: false,
+
+  props: {
+    varint: {
+      type: String,
+      default: 'primary',
+    },
+    to: {
+      type: Object as PropType<RouteLocationRaw>,
+      default: undefined,
+    },
+    tag: {
+      type: String,
+      default: 'button',
+    },
+    href: {
+      type: String,
+      default: undefined,
+    },
+    border: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  emits: ['click'],
+
+  setup(props, { emit }) {
+    const buttonType = computed<'button' | 'submit' | 'reset'>(() => 'button')
+
+    const componentName = computed(() => {
+      if (props.href) return 'a'
+      else if (props.to) return 'NuxtLink'
+      else return props.tag
+    })
+
+    const onClick = () => {
+      emit('click')
+    }
+    return {
+      buttonType,
+      componentName,
+      onClick,
+    }
+  },
+})
 </script>
 
 <template>
-  <div>
-    <button :type="buttonType" @click="onClick">
-      <slot />
-    </button>
-  </div>
+  <component
+    :is="componentName"
+    :type="buttonType"
+    v-bind="$attrs"
+    @click="onClick"
+  >
+    <slot />
+  </component>
 </template>
