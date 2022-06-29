d8f91405ac3cc48ac9b44e052ef5f0ad938859c0
junjie <1559304351@qq.com>
2022-06-29 14:42:50 +0800

    feat: add custom directive

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
   ssr: process.env.SSR !== 'false', // default: true
   modules: ['@vueuse/nuxt'],
+  plugins: ['@/plugins/directives.ts'],
   css: ['@/assets/sass/app.scss'],
   vite: {
     server: {
