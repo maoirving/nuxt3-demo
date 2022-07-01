1a7e8e628db73fbc2785b980260c64b931638f46
junjie <1559304351@qq.com>
2022-07-01 17:27:09 +0800

    feat: use counter store

--- components/base/Demo.vue
+++ components/base/Demo.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>base demo</div>
+  <div>
+    <p>base demo component</p>
+    <p>{{ counter }}</p>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useCounterStore } from '~~/store/counter'
+
+const demoStore = useCounterStore()
+const { counter } = storeToRefs(demoStore)
+</script>
 
 <style lang="scss" scoped></style>
--- pages/demo.vue
+++ pages/demo.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <h1>Demo Page</h1>
+    <p>{{ counter }}</p>
+    <BaseButton @click="counterStore.increment">add</BaseButton>
+    <BaseDemo />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useCounterStore } from '~~/store/counter'
+
+const counterStore = useCounterStore()
+const { counter } = counterStore
+</script>
 
 <style lang="scss" scoped></style>
