128bc30f6646ee837abfc764462677cedfc3a3f5
junjie <junjie@kooboo.net>
2022-12-05 10:32:37 +0800

    feat: add new demo

--- components/base/Demo.vue
+++ components/base/Demo.vue
@@ -13,4 +13,37 @@ const counterStore = useCounterStore()
 const { count } = storeToRefs(counterStore)
 </script>
 
+<!-- <script lang="ts">
+import { storeToRefs } from 'pinia'
+import { useCounterStore } from '~~/store/counter'
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+
+  emits: ['onChange'],
+
+  setup(props, { attrs, emit }) {
+    const counterStore = useCounterStore()
+    const { count } = storeToRefs(counterStore)
+
+    console.log(props)
+    console.log(attrs)
+
+    const onClick = () => {
+      emit('onChange')
+    }
+
+    return {
+      count,
+      onClick,
+    }
+  },
+})
+</script> -->
+
 <style lang="scss" scoped></style>
