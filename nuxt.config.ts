3c1aea959ce2bf9914e053e06a255aa4f0e968b9
junjie <junjie@kooboo.net>
2022-12-06 15:40:54 +0800

    chore: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: process.env.SSR !== 'false', // default: true
-  modules: ['@vueuse/nuxt'],
+  modules: ['@vueuse/nuxt', '@nuxtjs/tailwindcss'],
   buildModules: ['@pinia/nuxt'],
   plugins: ['@/plugins/directives.ts'],
   css: ['@/assets/sass/app.scss'],
